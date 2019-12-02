b81e59ca550561f7b1d44b3827ae50bb4c7db6ba
M. Gaumart Simeon <gaumart@insa-10224>
2019-12-02 13:21:21 +0100

    attribut account

--- Workspace-Eclipse/Projet-Messenger/src/Application/Account.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/Account.java
@@ -1,5 +1,56 @@
 package Application;
 
 public class Account {
+	
+	private String username;
+	
+	private String password;
+	
+	private Address address;
+	
+	public Account(String username_, String password_, Address address_) {
+		this.username = username_;
+		this.password = password_;
+		this.address = address_;
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAdress(Address address) {
+		this.address = address;
+	}
+	
+	
+	//A IMPLEMENTER//
+	public boolean verifyUnicityNewNickname(String NewNickname) {
+		
+		return false;
+	}
+	
+	//A IMPLEMENTER//
+	public boolean verifyPassword(String password) {
+		
+		return false;
+	}
+	
 
 }
