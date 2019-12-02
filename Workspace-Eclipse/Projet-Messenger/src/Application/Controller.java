4bb1e4062e4cf5c48c53ff81c29ffcc0efb9656a
M. Gaumart Simeon <gaumart@insa-10224>
2019-12-02 14:20:23 +0100

    UserInterface DBLocale contrloller - creation

--- Workspace-Eclipse/Projet-Messenger/src/Application/Controller.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/Controller.java
@@ -1,6 +1,25 @@
 package Application;
 
 public class Controller {
+	
+	private Account loggedAccount;
+	
+	//private ... server;
+	
+	private UserInterface userInterface;
+	
+	//private ... socket;
+	
+	private Conversation conversation;
+	
+	public Controller() {
+		
+		
+	}
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
