4ecbf02b3ba58419600cebb1d26fbb99981d8944
M. Boulou Valentin <boulou@insa-10225>
2019-12-02 13:07:04 +0100

    Ajout Address

--- Workspace-Eclipse/Projet-Messenger/src/Application/Address.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/Address.java
@@ -1,5 +1,28 @@
 package Application;
 
 public class Address {
+ private String IP;
+ private String NickName;
+ 
+protected Address(String ip, String nickname) {
+	IP = ip;
+	NickName = nickname;
+}
+ 
 
+protected String getIP() {
+	return IP;
+}
+
+protected void setIP(String iP) {
+	IP = iP;
+}
+
+protected String getNickName() {
+	return NickName;
+}
+
+protected void setNickName(String nickName) {
+	NickName = nickName;
+}
 }
