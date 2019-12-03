51a1c8491e68f10def3ee70d932d74320ce974eb
M. Boulou Valentin <boulou@insa-08132>
2019-12-03 17:54:38 +0100

    modif total de connected et disconnected, SEND MESSAGE FAUX

--- Workspace-Eclipse/Projet-Messenger/src/Application/InternalSocket.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/InternalSocket.java
@@ -5,15 +5,20 @@ import java.util.Iterator;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.io.IOException;
 import java.lang.Thread;
 import java.sql.Timestamp;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class InternalSocket implements NetworkSocketInterface {
 	ArrayList<Address> connectedUserList; // Need to be synchronized
-	protected static final int PORT_RCV = 6666;
-	protected static final int PORT_SEND = 6667;
+	protected static final int UDP_PORT_RCV = 6666;
+	protected static final int UDP_PORT_SEND = 6667;
 	protected static final int MAX_CHAR = 256;
 	protected static final String  CONNECTED =  "Connected";
 	protected static final String  DISCONNECTED =  "Disconnected";
@@ -26,27 +31,50 @@ public class InternalSocket implements NetworkSocketInterface {
 	public InternalSocket(String UsernameLoggedAccount_) throws SocketException{
 		this.UsernameLogged = UsernameLoggedAccount_;
 		System.out.println("InternalSocket: starting . . .");
-		this.Socket = new DatagramSocket(InternalSocket.PORT_SEND);
+		this.Socket = new DatagramSocket(InternalSocket.UDP_PORT_SEND);
 		
+		
+	}
+	
+	List<InetAddress> listAllBroadcastAddresses() throws SocketException {
+	    List<InetAddress> broadcastList = new ArrayList<>();
+	    Enumeration<NetworkInterface> interfaces 
+	      = NetworkInterface.getNetworkInterfaces();
+	    while (interfaces.hasMoreElements()) {
+	        NetworkInterface networkInterface = interfaces.nextElement();
+	 
+	        if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+	            continue;
+	        }
+	 
+	        networkInterface.getInterfaceAddresses().stream() 
+	          .map(a -> a.getBroadcast())
+	          .filter(Objects::nonNull)
+	          .forEach(broadcastList::add);
+	    }
+	    return broadcastList;
 	}
 
 	@Override
 	public void sendConnected(Account loggedAccount) {
-		// TODO Auto-generated method stub
-		InetAddress otherIP;
-		synchronized (connectedUserList) {
-			for (int i = 0; i < connectedUserList.size(); i++) {
-				
-				otherIP = connectedUserList.get(i).getIP();
-				String message = InternalSocket.CONNECTED.toString() + "\n" + loggedAccount.getUsername() + "\n" + loggedAccount.getPseudo() + "\n" + (new Timestamp(System.currentTimeMillis())).toString();;
-				DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),otherIP, InternalSocket.PORT_RCV);
-				try {
-					Socket.send(outPacket);
-				} catch( IOException e) {
-					System.out.println("Error Sending in send connected");
-				}
-			}
+
+		try {
+			this.Socket.setBroadcast(true);
+			
+		} catch (SocketException e) {
+			System.out.println("InternalSocket: Error sendConnected");
+			e.printStackTrace();
 		}
+		String message = InternalSocket.CONNECTED.toString() + "\n" + loggedAccount.getUsername() + "\n" + loggedAccount.getPseudo() + "\n" + (new Timestamp(System.currentTimeMillis())).toString();;
+		try {
+			DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),listAllBroadcastAddresses().get(0), InternalSocket.UDP_PORT_RCV);
+			this.Socket.send(outPacket);
+		} catch ( IOException e) {
+			System.out.println("InternalSocket: Error dans sendConnected");
+			e.printStackTrace();
+		}
+		
+		
 		
 		
 	}
@@ -79,7 +107,7 @@ public class InternalSocket implements NetworkSocketInterface {
 			}
 		}
 		String message = InternalSocket.MESSAGE.toString() + "\n" +  UsernameLogged + "\n" + Username + "\n" + msg.getTimestamp().toString() + "\n" + msg.getMsg();;
-		DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),res.getIP(), InternalSocket.PORT_RCV);
+		DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),res.getIP(), InternalSocket.UDP_PORT_RCV);
 		try {
 			Socket.send(outPacket);
 		} catch( IOException e) {
@@ -108,21 +136,29 @@ public class InternalSocket implements NetworkSocketInterface {
 
 	@Override
 	public void sendDisconnected(Account loggedAccount) {
-		// TODO Auto-generated method stub
-		InetAddress otherIP;
+		String message = InternalSocket.DISCONNECTED.toString() + "\n" + loggedAccount.getUsername() + "\n" + loggedAccount.getPseudo() + "\n" + (new Timestamp(System.currentTimeMillis())).toString();;
+		try {
+			DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),listAllBroadcastAddresses().get(0), InternalSocket.UDP_PORT_RCV);
+			this.Socket.send(outPacket);
+		} catch ( IOException e) {
+			System.out.println("InternalSocket: Error dans sendConnected");
+			e.printStackTrace();
+		}
+		
+		/*InetAddress otherIP;
 		synchronized (connectedUserList) {
 			for (int i = 0; i < connectedUserList.size(); i++) {
 				
 				otherIP = connectedUserList.get(i).getIP();
 				String message = InternalSocket.DISCONNECTED.toString() + "\n" + loggedAccount.getUsername() + "\n" + loggedAccount.getPseudo() + "\n" + (new Timestamp(System.currentTimeMillis())).toString();;
-				DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),otherIP, InternalSocket.PORT_RCV);
+				DatagramPacket outPacket = new DatagramPacket(message.getBytes(),message.length(),otherIP, InternalSocket.UDP_PORT_RCV);
 				try {
 					Socket.send(outPacket);
 				} catch( IOException e) {
 					System.out.println("Error Sending in send connected");
 				}
 			}
-		}
+		}*/
 	}
 
 }
@@ -132,7 +168,7 @@ class ThreadReceiver extends Thread {
 	
 	public ThreadReceiver() throws SocketException {
 		System.out.println("ThreadReceiver: starting . . .");
-		receiver = new DatagramSocket(InternalSocket.PORT_RCV);
+		receiver = new DatagramSocket(InternalSocket.UDP_PORT_RCV);
 		
 	}
 	
