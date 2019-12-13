b85ab9d5da66a442db954c0e762544afc8e0206a
M. Boulou Valentin <boulou@insa-10223>
2019-12-13 13:58:47 +0100

    MOdif message

--- Workspace-Eclipse/Projet-Messenger/src/Application/Message.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/Message.java
@@ -5,11 +5,13 @@ public class Message {
 	Boolean isEnvoyeur; // 1=envoyeur / 0=receveur
 	String msg;
 	Timestamp date;
+	Boolean isNew;
 	
-	protected Message(Boolean isSender , String msgs) {
+	protected Message(Boolean isSender , String msgs, Boolean _isNew) {
 		this.isEnvoyeur = isSender;
 		this.msg = msgs;
 		this.date = new Timestamp(System.currentTimeMillis());
+		this.isNew = _isNew;
 	}
 	
 	protected String getMsg() {
