0a6fdb6cb8ef7d99d24d93ada99922c24e2f8290
Gaumart Simeon <gaumart@insa-08129>
2020-02-04 18:57:58 +0100

    refresh page user co

--- Workspace-Eclipse/Projet-Messenger/src/Application/UserInterface.java
+++ Workspace-Eclipse/Projet-Messenger/src/Application/UserInterface.java
@@ -1101,9 +1101,14 @@ class UserInterface extends JFrame{
 			if(inMsgNonLu(sender.getUsername())==-1) {
 				UserMsgNonLu.add(sender);
 			}
+		}		
+		
+	}
+	
+	public void refreshPageUserCo() {
+		if (getContentPane().getComponents()[1].equals(scrollbar_uc)) {
+			setUtilisateursconnectesPage_same_frame();
 		}
-		
-		
 	}
 	
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
